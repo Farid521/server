eed5091de8c8dcfd674dc8b3b20397330baab425
Farid <faritfir521@gmail.com>
2023-06-17 00:25:57 +0700

    cors

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 30001;
+const cors = require("cors");
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 // Middleware untuk mengizinkan aplikasi Express membaca body dengan format JSON
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 // Endpoint untuk menangani permintaan POST dari URL https://happy-puce-ox.cyclic.app/mainPost
 app.post("/mainPost", (req, res) => {
