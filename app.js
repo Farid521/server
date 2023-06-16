6a5acde05f0fb81f3619af6c5f34c576855db5ae
Farid <faritfir521@gmail.com>
2023-06-16 23:30:07 +0700

    memperbaiki middleware

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Import middleware function
 const procces = require("./middleware/procces.js");
+
+app.use(express.json()); // Parse JSON request body
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
--- middleware/procces.js
+++ middleware/procces.js
@@ -1,5 +1,7 @@
 function middlewareFunction(request, response, next) {
-  console.log(request);
+  console.log(request.query);
   response.status(200).send("success");
   next();
 }
+
+module.exports = middlewareFunction;
